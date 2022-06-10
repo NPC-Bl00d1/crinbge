3d6df5b560669353d5383a715d66a133972412e1
Ishita L <79676329+NPC-Bl00d1@users.noreply.github.com>
2022-06-10 13:32:46 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function setup(){
+
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    
+    canvas = createCanvas(550, 500);
+    canvas.position(660, 150);
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+    }
+    
+    function draw(){
+    
+    background('#ceced6');
+    
+    }
+    
+    function modelLoaded(){
+    
+    console.log("wow it works");
+    
+    }
+    
+    function gotPoses(results){
+    
+    if(results.length > 0){
+        console.log(results);
+    }
+    }
